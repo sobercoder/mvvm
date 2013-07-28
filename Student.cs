5745b3b9204e34221cc2fc9ad25c3659f0b81dd9
Sandipan Das <sandipandas@mail.com>
2013-07-28 23:49:40 +0530

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApp.Model
+{
+  public class Student
+	{
+		[Key]
+		[MaxLength(10)]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string LastName { get; set; }
+
+		[Required]
+		public DateTime DateOfBirth { get; set; }
+	}
+}
